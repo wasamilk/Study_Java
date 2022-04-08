07293b8891fdb2c738b5c5fdcb704cef1ea3239c
wasamilk <vin5004@naver.com>
2022-04-08 17:29:21 +0900

    ~innerClass

---
+++ 05_study_java_etc/src/ch01_innerClass/LocalInnerTest.java
@@ -0,0 +1,43 @@
+package ch01_innerClass;
+
+class Outer {
+
+	int outNum = 100;
+	static int sNum = 200;
+
+	Runnable getRunnable(int i) {
+
+		int num = 100;
+
+		class MyRunnable implements Runnable {
+
+			int localNum = 10;
+
+			@Override
+			public void run() {
+				// run()이 다시 호출되었을 경우, num과 i는 getRunnable()의 지역변수여서 사라지고 없어서
+				// 
+				// num = 200; //에러 남. 지역변수는 상수로 바뀜
+				// i = 100;   //에러 남. 매개 변수 역시 지역변수처럼 상수로 바뀜
+				System.out.println("i =" + i);
+				System.out.println("num = " + num);
+				System.out.println("localNum = " + localNum);
+
+				System.out.println("outNum = " + outNum + "(외부 클래스 인스턴스 변수)");
+				System.out.println("Outter.sNum = " + Outer.sNum + "(외부 클래스 정적 변수)");
+			}
+		}
+		return new MyRunnable();
+	}
+}
+
+//지역 내부 클래스 
+public class LocalInnerTest {
+
+	public static void main(String[] args) {
+
+		Outer out = new Outer();
+		Runnable runner = out.getRunnable(10);
+		runner.run();
+	}
+}
